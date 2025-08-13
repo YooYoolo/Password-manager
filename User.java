003ead362bb99b72a976c8358ba556c1a6db3dcf
yotsume <yotsume52@gmail.com>
2025-08-13 15:54:56 +0300

    Add files via upload

---
+++ User.java
@@ -0,0 +1,39 @@
+package com.yotsume;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
+
+@Setter
+@Getter
+public class User {
+    String username;
+    SecretKey encryptionKey;
+
+    public User(String username) throws NoSuchAlgorithmException {
+        this.username = username;
+        this.encryptionKey = generateSecretKey();
+    }
+
+    private SecretKey generateSecretKey() throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(128);
+        return keyGenerator.generateKey();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return username.equals(user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return username.hashCode();
+    }
+}
